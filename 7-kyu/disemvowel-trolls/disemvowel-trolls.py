78c688425e2aac1bce20d93326d0813ec8050a44
Brandon <65471994+BearMSU@users.noreply.github.com>
2026-07-10 18:16:55 -0400

    Update solution - CodeHub

--- 7-kyu/disemvowel-trolls/disemvowel-trolls.py
+++ 7-kyu/disemvowel-trolls/disemvowel-trolls.py
@@ -1,4 +1,4 @@
 def disemvowel(string_):
     vowels = 'aeiouAEIOU'
-    new_string = ""
-    return new_string.join(char for char in string_ if char not in vowels)
+    
+    return "".join(char for char in string_ if char not in vowels)
