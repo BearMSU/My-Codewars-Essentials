a079d51b47373712cd630ce49e1aa42e119e4785
Brandon <65471994+BearMSU@users.noreply.github.com>
2026-09-13 15:41:20 -0400

    Update solution - CodeHub

--- javascript/8-kyu/multiplication-table-for-number/multiplication-table-for-number.js
+++ javascript/8-kyu/multiplication-table-for-number/multiplication-table-for-number.js
@@ -1,11 +1,4 @@
 function multiTable(number) {
   // good luck
-  let table = '';
-  
-  for(let i = 1; i <= 10; i++) {
-    table += `${i} * ${number} = ${i * number}${i < 10 ? '\n' : ''}`;
-  }
-  return table;
+  return [...Array(10)].map((_, i)=>`${i + 1} * ${number} = ${number * i + number}`).join('\n')
 }
-​
-​
